8e52041e54d5e046c912cca2d59efe149cf15bda
Blodjer <j95.szwaczka@gmail.com>
2019-02-14 23:07:51 +0100

    Minor delegate changes

--- Source/Delegate.h
+++ Source/Delegate.h
@@ -1,38 +1,45 @@
 #pragma once
 
+// Delegate with optional parameters
 template<typename... params>
 class Delegate
 {
-	typedef void(*Type)(void* callee, params...);
+	// Void function signature of the caller
+	using Signature = void(*)(void* object, params...);
 
-	void* Object;
-	Type FunctionCaller;
+	void* mObject; // generic pointer
+	Signature mCaller; // pointer to function
 
 public:
-	Delegate() {}
-	Delegate(void* callee, Type function) : Object(callee), FunctionCaller(function) {}
+	Delegate() = default;
+	Delegate(void* object, Signature caller) : mObject(object), mCaller(caller) {}
 
-	template <class T, void(T::*function)(params...)>
+	// F needs to be a member function of T and has to have the the same parameters as the declared delegate
+	template <class T, void(T::*F)(params...)>
 	static Delegate Create(T* object)
 	{
-		return Delegate(object, &Call<T, function>);
+		// Create delegate object and wrap the target function in the caller
+		return Delegate(object, &Call<T, F>);
 	}
 
-	void operator()(params... xs) const
+	void operator()(params... p) const
 	{
-		return (*this->FunctionCaller)(this->Object, xs...);
+		// Call the function template and feed it with the target object
+		(*this->mCaller)(this->mObject, p...);
 	}
 
 private:
-	template <class T, void(T::*function)(params...)>
-	static void Call(void* object, params... xs)
+	// Template wrapper to make the call
+	template <class T, void(T::*F)(params...)>
+	static void Call(void* object, params... p)
 	{
-		T* p = static_cast<T*>(object);
-		(p->*function)(xs...);
+		// Call the target function on the target object
+		T* const o = static_cast<T*>(object);
+		(o->*F)(p...);
 	}
 };
 
-#define FUNC_CONCAT(...) __VA_ARGS__
 #define DECLARE_DELEGATE(DelegateName, ...) typedef Delegate<__VA_ARGS__> DelegateName
-#define MAKE_DELEGATE(Delegate, Object, F) Delegate::Create<std::remove_pointer_t<decltype(Object)>, F>(Object)
-#define Create(Object, Function) Create<std::remove_pointer_t<decltype(Object)>, Function>(Object)
+//#define MAKE_DELEGATE(Delegate, Object, F) Delegate::Create<std::remove_pointer_t<decltype(Object)>, F>(Object)
+#define Create(Function) Create<std::remove_pointer_t<decltype(this)>, Function>(this)
+#define CreateExternal(Object, Function) Create<std::remove_pointer_t<decltype(Object)>, Function>(Object)
